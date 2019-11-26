8f4e6b68a14e2f1be4b1f0f1ab136c4f3e4a9a18
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-26 11:18:19 +0100

    opdracht

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,16 @@
 //javascript code
+
+function checkSaldo(){
+    var geldOpname = document.getElementById("geldOpname").value;
+
+    alert(geldOpname);
+
+    var bankSaldo = 1000;
+
+    var restTotaal = 1000 - geldOpname;
+
+    if (restTotaal < 25){
+        alert("sorry je hebt te weinig saldo")
+
+    }
+}
