99cadab4e0db658125dbce94c6413bc134f92401
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-26 13:21:32 +0100

    opdracht 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,17 @@
 //javascript code
+
+    
+var  myOperator = "+";
+var myNumber1 = prompt(" geef aub een getal op");
+var myNumber2 = prompt(" geef aub een getal op");
+var resultaat;
+
+if(myOperator == "+"){
+    resultaat = myNumber1 + myNumber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+}
+
+if (myOperator == "-"){
+    resultaat = myNumber1 - myNumber2;
+    document.getElementById("resultaat").innerHTML = resultaat;
+}
