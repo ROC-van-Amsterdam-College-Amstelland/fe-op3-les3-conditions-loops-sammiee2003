66abed456bb4637b8c52efb0ec0ce638ee8df8b8
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-26 12:30:03 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,20 @@
 //javascript code
+function checkSaldo(){
+
+    var geldOpname = document.getElementById("geldOpname").value;
+
+    if( geldOpname == 500){
+        alert("salaris is gestort")
+    }
+  
+    if(geldOpname > 500){
+        alert("er is bonus gestort")
+    }
+
+    if(geldOpname < 500){
+        alert("er is niks gestort")
+    }
+
+
+
+}
