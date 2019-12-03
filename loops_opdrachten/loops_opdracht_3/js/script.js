e62c3ed834aa061bab716c0f28c2f24d92a80eb0
sammiee2003 <samvandenbosch2003@gmail.com>
2019-12-03 13:01:05 +0100

    opdracht 3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,7 @@
 //javascript code
+for(var teller = 0; teller <= 50; teller++){
+    console.log(teller);
+    if (teller == 20 || teller == 40) {
+        console.warn("dit zijn mooie getallen")
+    }
+}
