14abdd6c9be98630971f887f4fe88b012754ea40
sammiee2003 <samvandenbosch2003@gmail.com>
2019-12-04 11:39:59 +0100

    java

--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,5 @@
 //javascript code
+for(var teller = 0; teller <= 1000; teller+= 10){
+    console.log(teller);
+   
+}
--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+for(var teller = 0; teller <= 20; teller ++){
+    
+    if(teller % 2 == 0){
+        document.getElementById("div").innerHTML += `is even ${teller} </BR>`
+
+    }
+    else{
+        console.log(teller + "is oneven ")
+    }
+    
+   
+}
